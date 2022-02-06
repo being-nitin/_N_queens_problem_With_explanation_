990c0f5894e1522167ef7d4ff3d9804284253ae6
Nitin <nsinghboss02@gmail.com>
2022-02-07 00:38:25 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,20 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// N queens problem, Based on backtracking.
+    /*
+    It means we have to find the paths which will be unique in a column ans row diagonally too.
+    like if there is a matrix of n*n then , in question it will be given that find n queens
+    for ex for 3*3 matrix find 3 queens
+       N * *
+       * * N
+       * N *
+     */
+
+
+    }
+
+
+}
